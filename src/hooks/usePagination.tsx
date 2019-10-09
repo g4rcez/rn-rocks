dae0da0d5c1f18600cd3f2bc4a9c734ba757d2e5
Garcez <allan.f.garcez@gmail.com>
2019-10-09 01:26:46 -0300

    usePagination hook

---
+++ src/hooks/usePagination.tsx
@@ -0,0 +1,80 @@
+import { useCallback, useEffect } from "react";
+import useReducer from "./useReducer";
+
+function paginate<T>(array: T[], itensPerView: number) {
+	const shallowCopy = [...array];
+	const size = array.length;
+	const pageSize = Math.ceil(size / itensPerView);
+	const page = [];
+	for (let i = 1; i <= pageSize; i++) {
+		page.push(shallowCopy.slice(i * pageSize, itensPerView));
+	}
+	return page;
+}
+
+const initialState = {
+	currentPage: 0,
+	paginateItens: [],
+	showGoBack: false,
+	showGoForward: false
+};
+
+type Actions = {
+	pages: any[];
+	showGoBack: boolean;
+	showGoForward: boolean;
+};
+type State = typeof initialState;
+const events = {
+	goBack(state: State) {
+		return { ...state, currentPage: state.currentPage - 1 };
+	},
+	goForward(state: State) {
+		return { ...state, currentPage: state.currentPage + 1 };
+	},
+	renderUpdate(state: State, action: Actions) {
+		return { ...state, paginateItens: action.pages, showGoBack: action.showGoBack, showGoForward: action.showGoForward };
+	},
+	setActions(state: State) {
+		const showGoBack = state.currentPage >= 1;
+		const showGoForward = state.currentPage < state.paginateItens.length;
+		return { ...state, showGoBack, showGoForward };
+	}
+};
+
+function usePagination<T>(itens: T[], itensPerView: number = 15) {
+	const [state, dispatch] = useReducer(initialState, events);
+
+	const goBack = useCallback(() => dispatch({ type: "goBack" }), []);
+
+	const goForward = useCallback(() => dispatch({ type: "goForward" }), []);
+
+	const greaterThanZero = itens.length > 0;
+
+	useEffect(() => {
+		if (greaterThanZero) {
+			const pages = paginate(itens, itensPerView);
+			const pagesSize = pages.length;
+			const showGoBack = state.currentPage >= 1;
+			const showGoForward = state.currentPage < pagesSize;
+			dispatch({ type: "renderUpdate", pages, showGoBack, showGoForward });
+		}
+	}, [itens.length]);
+
+	useEffect(() => {
+		dispatch({ type: "setActions" });
+	}, [state.currentPage]);
+
+	const { currentPage } = state;
+
+	return {
+		goBack,
+		goForward,
+		currentPage,
+		showGoBack: state.showGoBack,
+		showGoForward: state.showGoForward,
+		currentList: greaterThanZero ? state.paginateItens[currentPage] : []
+	};
+}
+
+export default usePagination;
