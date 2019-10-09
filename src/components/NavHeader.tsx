9d3dcb47269e39b9e765f772f59b90f53d0067e7
Garcez <allan.f.garcez@gmail.com>
2019-10-09 01:25:42 -0300

    Change Title to Text. Avoid cut some characters

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -1,7 +1,8 @@
-import { Body, Button, Header, Right, Left, Title, Icon } from "native-base";
+import { Body, Button, Header, Right, Left, Title, Icon, Text } from "native-base";
 import { Image } from "react-native";
 import React from "react";
 import Margin from "../helpers/Margin";
+import Colors from "../helpers/Colors";
 const Logo = require("../assets/logo.png");
 type Props = {
 	title: React.ReactNode;
@@ -16,7 +17,7 @@ const NavHeader = (props: Props) => {
 				<Image source={Logo} resizeMode="center" style={{ width: 28, height: 28 }} />
 			</Left>
 			<Body style={{ flex: 1, width: "100%" }}>
-				<Title>{props.title}</Title>
+				<Text style={{ color: Colors.white, fontSize: 18 }}>{props.title}</Text>
 			</Body>
 			<Right>
 				<Button transparent>
